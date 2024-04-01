b52db9ee5ab06effc28314fcc31b5356300aaa8d
Saurasish Basak <saurabasak90@gmail.com>
2024-04-01 23:02:28 +0530

    1st commit creating the base module

---
+++ Management-Backend/src/main/java/com/menu/app/management/ManagementBackendApplication.java
@@ -0,0 +1,13 @@
+package com.menu.app.management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagementBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManagementBackendApplication.class, args);
+	}
+
+}
---
+++ Management-Backend/src/test/java/com/menu/app/management/ManagementBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.menu.app.management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagementBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
